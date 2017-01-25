cf45ab3aa3221d72d1bbe38f5a05cb9aec299ac7
robrtsql <robrtsql@gmail.com>
2017-01-25 07:33:55 -0600

    Rename entity to avoid confusion with Unity GameObjects

--- World.cs
+++ World.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class World : MonoBehaviour {
 
-    private HashSet<WorldObject> objects;
+    private HashSet<WorldEntity> objects;
 
 	void Start () {
-        objects = new HashSet<WorldObject>();
+        objects = new HashSet<WorldEntity>();
 	}
 
-    public void Register(WorldObject worldObject)
+    public void Register(WorldEntity entity)
     {
-        objects.Add(worldObject);
+        objects.Add(entity);
     }
 
-    public void Deregesister(WorldObject worldObject)
+    public void Deregesister(WorldEntity entity)
     {
-        objects.Remove(worldObject);
+        objects.Remove(entity);
     }
 
     public Vector3 Move(Collider2D mover, Vector3 destination)
@@ -28,63 +28,63 @@ public class World : MonoBehaviour {
 
         while (enumerator.MoveNext())
         {
-            WorldObject worldObject = enumerator.Current;
-            resolvedDestination = ResolveIntersectionIfAny(mover, resolvedDestination, worldObject);
+            WorldEntity entity = enumerator.Current;
+            resolvedDestination = ResolveIntersectionIfAny(mover, resolvedDestination, entity);
         }
         return resolvedDestination;
     }
 
-    public Vector3 ResolveIntersectionIfAny(Collider2D mover, Vector3 resolvedDestination, WorldObject worldObject)
+    public Vector3 ResolveIntersectionIfAny(Collider2D mover, Vector3 destination, WorldEntity entity)
     {
         var moverBounds = mover.bounds;
-        var worldObjectBounds = worldObject.hitbox.bounds;
+        var entityBounds = entity.hitbox.bounds;
 
-        bool isLeft = moverBounds.max.x <= worldObjectBounds.min.x;
-        bool isRight = moverBounds.min.x >= worldObjectBounds.max.x;
-        bool isAbove = moverBounds.min.y >= worldObjectBounds.max.y;
-        bool isBelow = moverBounds.max.y <= worldObjectBounds.min.y;
+        bool isLeft = moverBounds.max.x <= entityBounds.min.x;
+        bool isRight = moverBounds.min.x >= entityBounds.max.x;
+        bool isAbove = moverBounds.min.y >= entityBounds.max.y;
+        bool isBelow = moverBounds.max.y <= entityBounds.min.y;
 
         bool isColliding = !isLeft && !isRight && !isAbove && !isBelow;
         bool isHorizontallyAligned = !isAbove && !isBelow;
         bool isVerticallyAligned = !isLeft && !isRight;
 
-        var destinationBounds = new Bounds(resolvedDestination + new Vector3(mover.offset.x, mover.offset.y), moverBounds.size);
+        var destinationBounds = new Bounds(destination + new Vector3(mover.offset.x, mover.offset.y), moverBounds.size);
 
-        bool isDestLeft = destinationBounds.max.x <= worldObjectBounds.min.x;
-        bool isDestRight = destinationBounds.min.x >= worldObjectBounds.max.x;
-        bool isDestAbove = destinationBounds.min.y >= worldObjectBounds.max.y;
-        bool isDestBelow = destinationBounds.max.y <= worldObjectBounds.min.y;
+        bool isDestLeft = destinationBounds.max.x <= entityBounds.min.x;
+        bool isDestRight = destinationBounds.min.x >= entityBounds.max.x;
+        bool isDestAbove = destinationBounds.min.y >= entityBounds.max.y;
+        bool isDestBelow = destinationBounds.max.y <= entityBounds.min.y;
 
         bool isDestColliding = !isDestLeft && !isDestRight && !isDestAbove && !isDestBelow;
 
         if (isDestColliding)
         {
-            Vector3 newResolvedDestination = new Vector3(resolvedDestination.x, resolvedDestination.y);
+            Vector3 newResolvedDestination = new Vector3(destination.x, destination.y);
             if (isHorizontallyAligned)
             {
                 if (isLeft)
                 {
-                    newResolvedDestination.x = worldObjectBounds.min.x - moverBounds.extents.x - mover.offset.x;
+                    newResolvedDestination.x = entityBounds.min.x - moverBounds.extents.x - mover.offset.x;
                     return newResolvedDestination;
                 } else if (isRight)
                 {
-                    newResolvedDestination.x = worldObjectBounds.max.x + moverBounds.extents.x - mover.offset.x;
+                    newResolvedDestination.x = entityBounds.max.x + moverBounds.extents.x - mover.offset.x;
                     return newResolvedDestination;
                 }
             } else if (true || isVerticallyAligned)
             {
                 if (isBelow)
                 {
-                    newResolvedDestination.y = worldObjectBounds.min.y - moverBounds.extents.y - mover.offset.y;
+                    newResolvedDestination.y = entityBounds.min.y - moverBounds.extents.y - mover.offset.y;
                     return newResolvedDestination;
                 }
                 else if (isAbove)
                 {
-                    newResolvedDestination.y = worldObjectBounds.max.y + moverBounds.extents.y - mover.offset.y;
+                    newResolvedDestination.y = entityBounds.max.y + moverBounds.extents.y - mover.offset.y;
                     return newResolvedDestination;
                 }
             }
         }
-        return resolvedDestination;
+        return destination;
     }
 }
--- WorldEntity.cs
+++ WorldEntity.cs
@@ -3,23 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof (Collider2D))]
-public class WorldObject : MonoBehaviour {
+public class WorldEntity : MonoBehaviour {
 
     [SerializeField]
-    private GameObject world;
-    private World worldComponent;
+    private World world;
 
     [HideInInspector]
     public Collider2D hitbox;
 
 	void Start () {
         hitbox = GetComponent<Collider2D>();
-        worldComponent = world.GetComponent<World>();
-        worldComponent.Register(this);
+        world.Register(this);
 	}
 
     void OnDestroy()
     {
-        worldComponent.Deregesister(this);
+        world.Deregesister(this);
     }
 }
